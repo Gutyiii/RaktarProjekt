5dbb110881c6ba709cec675bdf881ec15831907d
Vizsga SZF <vizsgaszf@oktatas.local>
2022-05-17 11:54:22 +0200

    main megírása

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,9 +1,24 @@
 package main;
 
+import java.time.LocalDate;
+import java.util.Collection;
+import modell.Elelmiszer;
+import modell.Raktar;
+
 public class Program {
 
     public static void main(String[] args) {
 
+        Raktar raktar = new Raktar();
+        raktar.felvesz(new Elelmiszer("üdítő", "Coca") {});
+        raktar.felvesz(new Elelmiszer("szörp", "Fanta") {});
+        raktar.felvesz(new Elelmiszer("alkohol", "Sprite") {});
+        //raktar.felvesz(new Elelmiszer("chipsz", "Chio", "2022-11-03") {});
+        raktar.getRaktar();
+        Collection.sort(raktar);
+        raktar.getRaktar();
+        Collection.sort(raktar, new GyartoComparator());
+        raktar.getRaktar();
     }
 
 }
